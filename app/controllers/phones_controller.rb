55034067eb6ce8ec3c2c23ba713d12ebd26d7028
Aravind Rajeshkanna <aravind.r@techmango.net>
2020-05-01 16:08:44 +0530

    Refactoring

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -8,7 +8,6 @@ class PhonesController < ApplicationController
 
   # POST /phones
   def create
-
     @phone = Phone.create(number: unique_number)
 
     render json: { phone: @phone.number }, status: :created
@@ -17,11 +16,15 @@ class PhonesController < ApplicationController
   private
 
   def unique_number
-    if params[:number].present? && params[:number].to_i.between?(1_111_111_111, 9_999_999_999)
+    if valid_number?
       return params[:number] unless Phone.exists?(number: params[:number])
     end
     number = rand(1_111_111_111..9_999_999_999)
     unique_number if Phone.exists?(number: number)
     number
+  end
+
+  def valid_number?
+    params[:number].present? && params[:number].to_i.between?(1_111_111_111, 9_999_999_999)
   end
 end
